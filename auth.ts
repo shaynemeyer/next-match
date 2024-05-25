85d8615cf8438ef991b79ccbfc3cc3299e5a3db3
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-25 07:14:03 -0700

    added id to session

--- auth.ts
+++ auth.ts
@@ -4,6 +4,14 @@ import authConfig from "./auth.config";
 import { prisma } from "./lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
