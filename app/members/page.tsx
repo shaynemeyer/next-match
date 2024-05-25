153c9d87d7054d4a749311bd3c0fdba0371d4d8c
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-25 10:33:38 -0700

    added members age

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,20 +1,15 @@
 import Link from "next/link";
 import React from "react";
 import { getMembers } from "../actions/memberActions";
+import MemberCard from "./MemberCard";
 
 async function MembersPage() {
   const members = await getMembers();
 
   return (
-    <div>
-      <ul>
-        {members &&
-          members.map((member) => (
-            <li key={member.id}>
-              <Link href={`/members/${member.id}`}>{member.name}</Link>
-            </li>
-          ))}
-      </ul>
+    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+      {members &&
+        members.map((member) => <MemberCard key={member.id} member={member} />)}
     </div>
   );
 }
