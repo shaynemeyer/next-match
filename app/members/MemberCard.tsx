4c882f0014a8cf110d2de7624086c3f755369d36
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-25 14:18:24 -0700

    added the like button

--- app/members/MemberCard.tsx
+++ app/members/MemberCard.tsx
@@ -1,3 +1,4 @@
+import LikeButton from "@/components/LikeButton";
 import { calculateAge } from "@/lib/util";
 import { Card, CardFooter, Image } from "@nextui-org/react";
 import { Member } from "@prisma/client";
@@ -18,6 +19,9 @@ function MemberCard({ member }: MemberCardProps) {
         src={member.image || "images/user.png"}
         className="aspect-square object-cover"
       />
+      <div className="absolute top-3 right-3 z-50">
+        <LikeButton targetId={member.userId} hasLiked={false} />
+      </div>
       <CardFooter className="flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
         <div className="flex flex-col text-white">
           <span className="font-semibold">
--- app/members/page.tsx
+++ app/members/page.tsx
@@ -7,7 +7,7 @@ async function MembersPage() {
   const members = await getMembers();
 
   return (
-    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
       {members &&
         members.map((member) => <MemberCard key={member.id} member={member} />)}
     </div>
