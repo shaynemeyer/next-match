c7a0e4c0ed9f17e5e134ce2f6a9797bb3b57b10d
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-26 19:25:36 -0700

    add edit photos page

---
+++ app/members/edit/photos/page.tsx
@@ -0,0 +1,36 @@
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
+import { CardBody, CardHeader, Divider, Image } from "@nextui-org/react";
+import React from "react";
+
+async function PhotosPage() {
+  const userId = await getAuthUserId();
+
+  const photos = await getMemberPhotosByUserId(userId);
+
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Edit Photos
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <div className="grid grid-cols-5 gap-3 p-5">
+          {photos &&
+            photos.map((photo) => (
+              <div className="relative" key={photo.id}>
+                <Image
+                  width={220}
+                  height={220}
+                  src={photo.url}
+                  alt="Image of user"
+                />
+              </div>
+            ))}
+        </div>
+      </CardBody>
+    </>
+  );
+}
+
+export default PhotosPage;
