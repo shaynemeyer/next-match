25be53b2c52efd1bc399fcf1e5deea111597fe78
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-28 07:13:03 -0700

    added message sidebar

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import MessageSidebar from "./MessageSidebar";
 
 function MessagesPage() {
-  return <div>MessagesPage</div>;
+  return (
+    <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
+      <div className="col-span-2">
+        <MessageSidebar />
+      </div>
+      <div className="col-span-10">Message table goes here</div>
+    </div>
+  );
 }
 
 export default MessagesPage;
