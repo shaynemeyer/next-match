260a8459c24dff271a161521ca2e45d609430f17
Shayne Meyer <shayne.meyer@gmail.com>
2024-05-31 06:23:17 -0700

    added join private channel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,25 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
 import TopNav from "@/components/navbar/TopNav";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Next Match",
   description: "A Dating App for the Next Generation",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  const userId = session?.user?.id || null;
+
   return (
     <html lang="en">
       <body>
-        <Providers>
+        <Providers userId={userId}>
           <TopNav />
           <main className="container mx-auto">{children}</main>
         </Providers>
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import { useNotificationChannel } from "@/hooks/useNotificationChannel";
 import { usePresenceChannel } from "@/hooks/usePresenceChannel";
 import { NextUIProvider } from "@nextui-org/react";
 import React, { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function Providers({ children }: { children: ReactNode }) {
+function Providers({
+  children,
+  userId,
+}: {
+  children: ReactNode;
+  userId: string | null;
+}) {
   usePresenceChannel();
+  useNotificationChannel(userId);
+
   return (
     <NextUIProvider>
       <ToastContainer
