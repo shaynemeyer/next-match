c233b3e3562e6a4fb76c08d88d772183b3ef9335
Shayne Meyer <shayne.meyer@gmail.com>
2024-06-01 12:49:21 -0700

    added filters component topnav

--- components/navbar/TopNav.tsx
+++ components/navbar/TopNav.tsx
@@ -1,10 +1,4 @@
-import {
-  Button,
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-} from "@nextui-org/react";
+import { Button, Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
 import { GiMatchTip } from "react-icons/gi";
@@ -12,61 +6,65 @@ import NavLink from "./NavLink";
 import { auth } from "@/auth";
 import UserMenu from "./UserMenu";
 import { getUserInfoForNav } from "@/app/actions/userActions";
+import Filters from "./Filters";
 
 async function TopNav() {
   const session = await auth();
   const userInfo = session?.user && (await getUserInfoForNav());
 
   return (
-    <Navbar
-      maxWidth="xl"
-      className="bg-gradient-to-r from-purple-400 to-purple-700"
-      classNames={{
-        item: [
-          "text-xl",
-          "text-white",
-          "uppercase",
-          "data-[active=true]:text-yellow-200",
-        ],
-      }}
-    >
-      <NavbarBrand as={Link} href="/">
-        <GiMatchTip size={40} className="text-gray-200" />
-        <div className="font-bold text-3xl flex">
-          <span className="text-gray-900">Next</span>
-          <span className="text-gray-200">Match</span>
-        </div>
-      </NavbarBrand>
-      <NavbarContent justify="center">
-        <NavLink href="/members" label="Matches" />
-        <NavLink href="/lists" label="Lists" />
-        <NavLink href="/messages" label="Messages" />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        {userInfo ? (
-          <UserMenu userInfo={userInfo} />
-        ) : (
-          <>
-            <Button
-              as={Link}
-              href="/login"
-              variant="bordered"
-              className="text-white"
-            >
-              Login
-            </Button>
-            <Button
-              as={Link}
-              href="/register"
-              variant="bordered"
-              className="text-white"
-            >
-              Register
-            </Button>
-          </>
-        )}
-      </NavbarContent>
-    </Navbar>
+    <>
+      <Navbar
+        maxWidth="xl"
+        className="bg-gradient-to-r from-purple-400 to-purple-700"
+        classNames={{
+          item: [
+            "text-xl",
+            "text-white",
+            "uppercase",
+            "data-[active=true]:text-yellow-200",
+          ],
+        }}
+      >
+        <NavbarBrand as={Link} href="/">
+          <GiMatchTip size={40} className="text-gray-200" />
+          <div className="font-bold text-3xl flex">
+            <span className="text-gray-900">Next</span>
+            <span className="text-gray-200">Match</span>
+          </div>
+        </NavbarBrand>
+        <NavbarContent justify="center">
+          <NavLink href="/members" label="Matches" />
+          <NavLink href="/lists" label="Lists" />
+          <NavLink href="/messages" label="Messages" />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {userInfo ? (
+            <UserMenu userInfo={userInfo} />
+          ) : (
+            <>
+              <Button
+                as={Link}
+                href="/login"
+                variant="bordered"
+                className="text-white"
+              >
+                Login
+              </Button>
+              <Button
+                as={Link}
+                href="/register"
+                variant="bordered"
+                className="text-white"
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </NavbarContent>
+      </Navbar>
+      <Filters />
+    </>
   );
 }
 
