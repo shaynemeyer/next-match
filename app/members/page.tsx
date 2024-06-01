77224172a39b68b79d89859c8c9af646d62b83df
Shayne Meyer <shayne.meyer@gmail.com>
2024-06-01 14:42:58 -0700

    added and styled pagination component

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { getMembers } from "../actions/memberActions";
 import MemberCard from "./MemberCard";
 import { fetchCurrentUserLikeIds } from "../actions/likeActions";
+import PaginationComponent from "@/components/PaginationComponent";
 
 async function MembersPage() {
   const members = await getMembers();
   const likeIds = await fetchCurrentUserLikeIds();
 
   return (
-    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
-      {members &&
-        members.map((member) => (
-          <MemberCard key={member.id} member={member} likeIds={likeIds} />
-        ))}
-    </div>
+    <>
+      <div className="mt-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
+        {members &&
+          members.map((member) => (
+            <MemberCard key={member.id} member={member} likeIds={likeIds} />
+          ))}
+      </div>
+      <PaginationComponent />
+    </>
   );
 }
 
