de52daced5140a8f9338a7e061c913d417b4c375
Shayne Meyer <shayne.meyer@gmail.com>
2024-06-05 06:48:47 -0700

    added approval and reject functionality for photo moderation

--- app/admin/moderation/page.tsx
+++ app/admin/moderation/page.tsx
@@ -1,4 +1,15 @@
-function PhotoModerationPage() {
-  return <div>Photo Moderation</div>;
+import { getUnapprovedPhotos } from "@/app/actions/adminActions";
+import MemberPhotos from "@/components/MemberPhotos";
+import { Divider } from "@nextui-org/react";
+
+async function PhotoModerationPage() {
+  const photos = await getUnapprovedPhotos();
+  return (
+    <div className="flex flex-col mt-10 gap-3">
+      <h3 className="text-2xl">Photos awaiting moderation</h3>
+      <Divider />
+      <MemberPhotos photos={photos} />
+    </div>
+  );
 }
 export default PhotoModerationPage;
--- components/MemberImage.tsx
+++ components/MemberImage.tsx
@@ -3,14 +3,42 @@
 import { Photo } from "@prisma/client";
 import { CldImage } from "next-cloudinary";
 import React from "react";
-import { Image } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 import clsx from "clsx";
+import { useRole } from "@/hooks/useRole";
+import { ImCheckmark, ImCross } from "react-icons/im";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { approvePhoto, rejectPhoto } from "@/app/actions/adminActions";
 
 type MemberImageProps = {
   photo: Photo | null;
 };
 
 function MemberImage({ photo }: MemberImageProps) {
+  const role = useRole();
+  const router = useRouter();
+
+  if (!photo) return null;
+
+  const approve = async (photoId: string) => {
+    try {
+      await approvePhoto(photoId);
+      router.refresh();
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
+  const reject = async (photo: Photo) => {
+    try {
+      await rejectPhoto(photo);
+      router.refresh();
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div>
       {photo?.publicId ? (
@@ -22,7 +50,7 @@ function MemberImage({ photo }: MemberImageProps) {
           crop="fill"
           gravity="faces"
           className={clsx("rounded-2xl", {
-            "opacity-40": !photo.isApproved,
+            "opacity-40": !photo.isApproved && role !== "ADMIN",
           })}
           priority
         />
@@ -34,11 +62,31 @@ function MemberImage({ photo }: MemberImageProps) {
           alt="Image of user"
         />
       )}
-      {!photo?.isApproved && (
+      {!photo?.isApproved && role !== "ADMIN" && (
         <div className="absolute bottom-2 w-full bg-slate-200 p-1">
           <div className="flex justify-center text-danger font-semibold">
             Awaiting approval
           </div>
+        </div>
+      )}
+      {role === "ADMIN" && (
+        <div className="flex flex-row gap-2 mt-2">
+          <Button
+            onClick={() => approve(photo.id)}
+            color="success"
+            variant="bordered"
+            fullWidth
+          >
+            <ImCheckmark size={20} />
+          </Button>
+          <Button
+            onClick={() => reject(photo)}
+            color="danger"
+            variant="bordered"
+            fullWidth
+          >
+            <ImCross size={20} />
+          </Button>
         </div>
       )}
     </div>
